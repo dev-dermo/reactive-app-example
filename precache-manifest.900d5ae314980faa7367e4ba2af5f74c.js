1e5cc1bf4c1388274c39a1774aa5f720ab969a4f
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-07-21 00:24:05 -0700

    Updates

---
+++ precache-manifest.900d5ae314980faa7367e4ba2af5f74c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0fc2a6a1ddb79c10cceed678bfd07a",
+    "url": "/teachee-reactive/index.html"
+  },
+  {
+    "revision": "87d0e6ef68868e24d951",
+    "url": "/teachee-reactive/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "750b0bd583f19c13a303",
+    "url": "/teachee-reactive/static/css/main.e4aa857f.chunk.css"
+  },
+  {
+    "revision": "87d0e6ef68868e24d951",
+    "url": "/teachee-reactive/static/js/2.50b314bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/teachee-reactive/static/js/2.50b314bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750b0bd583f19c13a303",
+    "url": "/teachee-reactive/static/js/main.28e89688.chunk.js"
+  },
+  {
+    "revision": "073191ac24eb9da3c668",
+    "url": "/teachee-reactive/static/js/runtime-main.9343fcac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.984117fdcbdb188a11b40433da042d97.js"
+  "/teachee-reactive/precache-manifest.900d5ae314980faa7367e4ba2af5f74c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teachee-reactive/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
